01dfd6b250bdc6898240563efa2f50a29bae7b28
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-17 15:23:10 +0300

    task5

--- task_5.sql
+++ task_5.sql
@@ -1,9 +1,9 @@
 -- task_5.sql
--- إدخال صف واحد في جدول Customers لقاعدة البيانات alx_book_store
+-- إدخال صف واحد في جدول customer لقاعدة البيانات alx_book_store
 
 -- اختيار قاعدة البيانات الصحيحة
 USE alx_book_store;
 
 -- إدخال البيانات
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');

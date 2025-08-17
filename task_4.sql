178a2b1be2666426b5cab368dbda5557b4c28d38
youssifsaid1 <youssifsaid101@gmail.com>
2025-08-17 15:20:12 +0300

    11

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+-- task_4.sql
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
